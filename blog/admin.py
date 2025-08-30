192bb0445f6ea1a5f00e3864d275a399a8ab0a7e
MaxPlayFire <148088491+MaxPlayFire@users.noreply.github.com>
2025-08-30 13:51:35 +0300

    delo cdelano

--- blog/admin.py
+++ blog/admin.py
@@ -5,12 +5,24 @@ from .models import Author, Post, Comment
 
 class CommentInline(admin.TabularInline):
     model = Comment
-    extra = 1
+    extra = 0
+    readonly_fields = ("created_date",)
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "published_date", "published_recently")
+    list_display = ("title", "author", "published_date", "is_recent")
+    list_filter = ("author", "published_date")
+    search_fields = ("title", "content")
     inlines = [CommentInline]
 
-admin.site.register(Author)
-admin.site.register(Post, PostAdmin)
-admin.site.register(Comment)
+    @admin.display(boolean=True, ordering="published_date", description="≤ 7 днів")
+    def is_recent(self, obj):
+        return obj.published_recently()
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author_name", "post", "created_date")
+    list_filter = ("created_date", "post")
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Author
 
-# Create your views here.
-
 def post_list(request):
-    posts = Post.objects.all().order_by('-published_date')
-    return render(request, 'post_list.html', {'posts': posts})
+    posts = Post.objects.all().order_by("-published_date")
+    return render(request, "blog/post_list.html", {"posts": posts})
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
